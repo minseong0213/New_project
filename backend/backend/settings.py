892f94c2fb3bc48200accfb8786cf65f639575fe
minseong0213 <minseong0213@naver.com>
2023-04-16 02:00:23 +0900

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,3 +123,27 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#EMAIL 전송
+#메일을 호스트하는 서버
+EMAIL_HOST = 'smtp.gmail.com'
+
+# GMAIL과의 통신하는 포트
+EMAIL_PORT = '587'
+
+#발신할 이메일
+#EMAIL_HOST_USER = '구글아이디@gmail.com'
+EMAIL_HOST_USER = '(#본인 아이디)@gmail.com'
+
+#발신할 메일의 비밀번호
+#EMAIL_HOST_PASSWORD = '구글비밀번호'
+EMAIL_HOST_PASSWORD = '구글비밀번호'
+
+#TLS 보안 방법
+EMAIL_USE_TLS = True
+
+#사이트와 관련한 자동응답을 받을 이메일 주소 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
